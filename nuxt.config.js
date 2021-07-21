bd3901c79e8608172fd65fd3c151db904620ebf4
papermashea <molloy.shea@gmail.com>
2021-07-21 12:57:36 -0400

    pulling original package.json and nuxt.config.js files from github, commenting out what was in the new ones

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,72 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b1ae5ba55e4d7a1b9e8268f7936b2be58a3ef240c5efc877335a8de27c3a24af
-size 1336
+// version https://git-lfs.github.com/spec/v1
+// oid sha256:b1ae5ba55e4d7a1b9e8268f7936b2be58a3ef240c5efc877335a8de27c3a24af
+// size 1336
+
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+    '@/plugins/element-ui',
+    // {
+    //   src: '~plugins/index.js',
+    //   ssr: false
+    // }
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
+
